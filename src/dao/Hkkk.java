d8b7d6472ea6df0ed893f0d71ee8e268df17042d
Li Shu Yong <2293778921@qq.com>
2020-03-09 16:37:52 +0800

    第一次

---
+++ src/dao/Hkkk.java
@@ -0,0 +1,27 @@
+package dao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author lishuyong
+ * @creat 2020-03-09-16:33
+ */
+@WebServlet("/KK")
+public class Hkkk extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.print("hshah");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
